0591d2e1381bcd1e0b5117910cca52c59cd1ab41
musfan1 <musfan1@mail.ru>
2023-06-16 15:14:34 +0300

    5.1. Зона видимости переменных [#180147]

--- src/main/java/ru/job4j/max/Reduce.java
+++ src/main/java/ru/job4j/max/Reduce.java
@@ -8,8 +8,8 @@ public class Reduce {
     }
 
     public void print() {
-        for (int i : array) {
-            System.out.println(i);
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
         }
     }
 

4f0dca6bf2d2a9052f3b261987cf31c384e1938a
musfan1 <musfan1@mail.ru>
2023-06-16 10:06:13 +0300

    6. Правила Checkstyle [#504981 #423303]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -4,24 +4,24 @@ public class Broken {
 
     public static final String NEW_VALUE = "";
 
-    public String surname;
+    private String surname;
 
-    String name;
+    private String name;
 
     private int size = 10;
 
-    Broken() {
+    public Broken() {
     }
 
-    void echo() {
+    public void echo() {
     }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 }

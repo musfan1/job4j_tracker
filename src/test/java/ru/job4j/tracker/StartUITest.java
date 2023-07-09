c16c95e2641234e6344867174f4661a4e7a0aaa4
musfan1 <musfan1@mail.ru>
2023-07-09 12:14:43 +0300

    9. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 class StartUITest {
 
     @Test
@@ -15,5 +17,32 @@ class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Первый");
         assertThat(created.getName()).isEqualTo(expected.getName());
+    }
+
+    @Test
+    void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Первый");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "Второй"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("Второй");
+    }
+
+    @Test
+    void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Третий");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId())
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted);
     }
 }

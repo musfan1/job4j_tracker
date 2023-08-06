c9334f99e751bbecbdab4ea4a777a2d4d65371e8
musfan1 <musfan1@mail.ru>
2023-08-06 12:42:53 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>(List.of(origin));
+        for (String word : text) {
+            if (!check.contains(word)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}

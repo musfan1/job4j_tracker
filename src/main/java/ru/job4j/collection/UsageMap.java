7e891fc68ec4f91e809d39e1f1a4d88f84d17859
musfan1 <musfan1@mail.ru>
2023-08-15 09:32:25 +0300

    0. Map, HashMap [#212652 #436896]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("ivan@mail.ru", "Ivan Ivanov");
+        map.put("ivan@mail.ru", "Ivan Ivanov");
+        map.put("ivan@mail.ru", "Ivan Ivanov");
         map.put("petr@yandex.ru", "Petr Petrov");
+        map.put("petr@yandex.ru", "Sidor Sidorov");
         map.put("john@gmail.com", "John Johns");
         for (String key : map.keySet()) {
             System.out.println(key + " " + map.get(key));

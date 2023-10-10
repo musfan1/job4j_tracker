c8d58f326d358c81c54f401ea3738e3d4b340e3b
musfan1 <musfan1@mail.ru>
2023-10-10 15:32:26 +0300

    3. Отсортировать департаменты [#34352 #451461]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,22 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int rsl = 0;
         String[] s1 = o1.split("/");
         String[] s2 = o2.split("/");
-        for (int i = 0; i < Math.min(s1.length, s2.length); i++) {
-            if (i == 0) {
-                rsl = s2[i].compareTo(s1[i]);
-            } else {
-                rsl = s1[i].compareTo(s2[i]);
-            }
-            if (rsl != 0) {
-                break;
-            }
-        }
-        if (rsl == 0) {
-            rsl = Integer.compare(s1.length, s2.length);
-        }
-        return rsl;
+        int rsl = s2[0].compareTo(s1[0]);
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
